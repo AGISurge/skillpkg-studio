a6bb5fd72ca7ac01a79a8bbeb3ce6800bb0a0af1
zhouqc <634545283@qq.com>
2026-04-16 18:21:34 +0800

    feat:增加elct

--- main.js
+++ main.js
@@ -1,26 +1,32 @@
-const { app, BrowserWindow } = require('electron/main')
-
+const { app, BrowserWindow } = require('electron/main');
+const isDev = require('electron-is-dev');
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  const urlLocation = isDev
+    ? 'http://localhost:3000'
+    : `file://${__dirname}/index.html`;
+  // win.loadFile('index.html');
+  win.loadURL(urlLocation);
+};
 
-  win.loadFile('index.html')
-}
-
-app.whenReady().then(() => {
-  createWindow()
+app.on('ready', () => {
+  createWindow();
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
+      createWindow();
     }
-  })
-})
+  });
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
