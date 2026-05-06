4441ac12e13b5c8651cd3a4af9f02c71a833c835
Ironben <hey@ironben.dev>
2026-05-06 10:21:23 +0800

    🔧 chore(craco.config.js, tsconfig.json)：更新配置以忽略特定警告和弃用

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,16 @@ module.exports = {
     alias: {
       "@": path.resolve(__dirname, "src"),
     },
+    configure: (webpackConfig) => {
+      webpackConfig.ignoreWarnings = [
+        ...(webpackConfig.ignoreWarnings || []),
+        (warning) =>
+          warning.module?.resource?.includes("node_modules/@griffel/") &&
+          warning.message?.includes("Failed to parse source map"),
+      ];
+
+      return webpackConfig;
+    },
   },
   jest: {
     configure: {
